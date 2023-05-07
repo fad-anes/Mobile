f1f5b54c7bc3057030d4dbf8a561ddd17a03488a
Abdessalam10 <abdessalam.bahri@esprit.tn>
2023-05-07 18:02:50 +0100

    add a filter for users

--- src/gui/UsersList.java
+++ src/gui/UsersList.java
@@ -9,6 +9,7 @@ import com.codename1.io.ConnectionRequest;
 import com.codename1.io.NetworkEvent;
 import com.codename1.io.NetworkManager;
 import com.codename1.ui.Button;
+import com.codename1.ui.ComboBox;
 import com.codename1.ui.Command;
 import com.codename1.ui.Container;
 import com.codename1.ui.Dialog;
@@ -24,8 +25,10 @@ import com.codename1.ui.URLImage;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.*;
 import com.mycompany.myapp.MyApplication;
+import entites.Role;
 import entites.Users;
 import java.io.IOException;
+import services.RoleService;
 
 import services.UserService;
 
@@ -34,10 +37,12 @@ import services.UserService;
  * @author abdes
  */
 public class UsersList extends Form{
-  public static  List<Users> list ;
+  public static  List<Role> list ;
+  Container main ;
     public UsersList(){
     setTitle("liste de users");
-   setLayout(BoxLayout.y());
+   //setLayout(BoxLayout.y());
+   main = new Container(BoxLayout.y());
          Toolbar toolbar = new Toolbar();
       setToolBar(toolbar);
      // set the toolbar for the form
@@ -53,9 +58,49 @@ toolbar.addCommandToLeftSideMenu("TuniTask",null,(e) -> new Aceuil().show());
         toolbar.addCommandToLeftSideMenu("Log out ",null,(e) -> new Aceuil().show());
     Label title = new Label("TuniTask");
 toolbar.setTitleComponent(title);
+ String[] roles = {"Client", "Freelancer","Admin"};
+    ComboBox<String> roleComboBox = new ComboBox<>(roles);
+     Container cn = new Container(BoxLayout.x());
+     Button  filter = new Button("filter");
+     
+        cn.addAll(roleComboBox,filter);
+        add(cn);
+      filter.addActionListener(evt -> {
+          //Dialog.show("Connection", "You have to make an account!"  , "OK", null);
+          removeComponent(main);
+            main = new Container(BoxLayout.y());
+      
+       
+                RoleService rs = new RoleService();
+               
+                 ArrayList<Role> list1 =rs.parseFilter((ArrayList<Role>)UsersList.list ,roleComboBox.getSelectedItem());
+      
 
-  ConnectionRequest req = new ConnectionRequest();
-        String url = "http://127.0.0.1:8000/controle/json/userlist";
+               for (int i=0; i<list1.size();i++)
+                  {
+                         //addElement(UsersList.list.get(i));
+                      System.out.println("ok"+list.get(i).getIdUser().getId());
+                            String url ="http://127.0.0.1/img/"+ list.get(i).getIdUser().getSrcimage();
+        System.out.println(url);
+      Image img = MyApplication.theme.getImage("logo.png");
+    EncodedImage enc = EncodedImage.createFromImage(img,false);
+   img=URLImage.createToStorage(enc,list.get(i).getIdUser().getSrcimage(),url).scaled(300, 400);
+    ImageViewer imgp= new ImageViewer(img);
+   
+   
+       main.addAll( addElement(list1.get(i)),imgp);
+    
+    
+                  }
+
+                add(main);
+            }
+            
+        );
+      
+
+       ConnectionRequest req = new ConnectionRequest();
+        String url = "http://127.0.0.1:8000/controle/json/roles";
         System.out.println(url);
         //ConnectionRequest.setCertificateValidation(false);
 
@@ -67,24 +112,24 @@ toolbar.setTitleComponent(title);
                 Boolean result = req.getResponseCode() == 200; //Code HTTP 200 OK
                      String response = new String(req.getResponseData());
                 req.removeResponseListener(this);
-                UserService us = new UserService();
-                System.out.println(us.parseUsers(response));
-                 UsersList.list =us.parseUsers(response);
+                RoleService rs = new RoleService();
+                System.out.println(rs.parseRoles1(response));
+                 UsersList.list =rs.parseRoles1(response);
       
 
                for (int i=0; i<UsersList.list.size();i++)
                   {
                          //addElement(UsersList.list.get(i));
-                      System.out.println("ok"+list.get(i).getId());
-                            String url ="http://127.0.0.1/img/"+ list.get(i).getSrcimage();
+                      System.out.println("ok"+list.get(i).getIdUser().getId());
+                            String url ="http://127.0.0.1/img/"+ list.get(i).getIdUser().getSrcimage();
         System.out.println(url);
       Image img = MyApplication.theme.getImage("logo.png");
     EncodedImage enc = EncodedImage.createFromImage(img,false);
-   img=URLImage.createToStorage(enc,list.get(i).getSrcimage(),url).scaled(300, 400);
+   img=URLImage.createToStorage(enc,list.get(i).getIdUser().getSrcimage(),url).scaled(300, 400);
     ImageViewer imgp= new ImageViewer(img);
      Button btn = new Button("block");
    
-       addAll( addElement(list.get(i)),imgp,btn);
+       main.addAll( addElement(list.get(i)),imgp,btn);
     
     
                   }
@@ -96,24 +141,24 @@ toolbar.setTitleComponent(title);
         NetworkManager.getInstance().addToQueue(req);
    
 
- 
+     addAll(main);
 
     }
-    public Container addElement (Users u){
+    public Container addElement (Role u){
   
     
    
      Container cn1 = new Container (BoxLayout.x());
      //ImageViewer imgUser=new ImageViewer(MyApplication.theme.getImage("logo.png").scaled(300, 400));
      Container cn2= new Container (BoxLayout.y());
-     Label email= new Label(u.getEmail());
-       Label name= new Label(u.getLastName()+"\t"+" "+ "\t"+u.getFirstName());
+     Label email= new Label(u.getIdUser().getEmail());
+       Label name= new Label(u.getIdUser().getLastName()+"\t"+" "+ "\t"+u.getIdUser().getFirstName());
        // System.out.println(Integer.toString(u.getId()));
-     Label id= new Label(Integer.toString(u.getId()));  
-    
+     Label id= new Label(Integer.toString(u.getIdUser().getId()));  
+     Label role= new Label(u.getRoleName()); 
     // email.setSize(new Dimension(10,10));
      id.setSize(new Dimension(10,10));
-     cn2.addAll(id,email,name);
+     cn2.addAll(id,email,name,role);
     
      cn1.addAll(cn2);
       return cn1;
